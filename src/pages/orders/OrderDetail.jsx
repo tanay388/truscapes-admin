43634b4dd240427d2e9c112fde4d50a1a2eec9cb
tanay388 <tanay.deo388@gmail.com>
2025-01-21 13:19:13 +0530

    Fix: order sytem

--- src/pages/orders/OrderDetail.jsx
+++ src/pages/orders/OrderDetail.jsx
@@ -173,6 +173,9 @@ const OrderDetail = () => {
                     <div>
                       <h3 className="font-medium">{item.product?.name}</h3>
                       <p className="text-sm text-gray-500">
+                        Variant: {item.variant.name}
+                      </p>
+                      <p className="text-sm text-gray-500">
                         Quantity: {item.quantity}
                       </p>
                       <p className="text-sm text-gray-500">
